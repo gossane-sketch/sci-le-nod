c3656db1519eaff4ccdb85322b97fd107cb790d9
gossane-sketch <gossane@gmail.com>
2026-04-26 20:15:16 +0200

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: { extend: { colors: { nid: { warm: '#8B5E3C', accent: '#C4873B', green: '#4A7C59' } } } },
+  plugins: [],
+}
